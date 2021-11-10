eeb2459bb162f91060b570ea0584e02e0fdabc63
t4rcisio <tarcisiobatistaprates@gmail.com>
2021-11-10 19:44:08 -0300

    cluster

---
+++ pages/api/cluster.js
@@ -0,0 +1,24 @@
+function badRequest(res){
+    res.status(400).json({error: "Bad request"})
+    return
+}
+
+var resoponse
+
+export default function(req, res){
+    
+    if(req.method == "POST"){
+        const {sshd} = req.body
+        if(!sshd){
+            badRequest(res)
+        }
+
+        resoponse = sshd
+        res.status(200).json({sshd: sshd})
+    }
+    if(req.method == "GET"){
+        res.status(200).json({sshd: resoponse})
+    }else{
+        badRequest(res)
+    }
+}
